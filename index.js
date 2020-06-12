d2aec307edfb2874f8cd944ac7eddead45f892e4
Matthew O'Gorman <mog@rldn.net>
2020-06-12 12:58:04 -0400

    add sentry support to lambda

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const lambda = require("./src/lambda.js");
+const Sentry = require("@sentry/node");
 
 ("use strict");
 
-exports.handler = function (event, context) {
+function initHandler(lambdaHandler) {
   if (process.env.NODE_ENV !== "prod" && process.env.NODE_ENV !== "dev") {
     require("dotenv").config();
   }
+  const dsn = process.env.SENTRY;
+  Sentry.init({ dsn: dsn });
 
+  return async (event, context) => {
+    try {
+      return await lambdaHandler(event, context);
+    } catch (error) {
+      Sentry.captureException(error);
+      await Sentry.flush(2000);
+    }
+    context.succeed({ status: 500 });
+  };
+}
+
+exports.handler = initHandler((event, context) => {
   return lambda.run(event, context);
-};
+});
