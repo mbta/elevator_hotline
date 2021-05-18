dd7ecd07025c5d5d4b49999bfecc870c1744d81e
Paul Swartz <paul@paulswartz.net>
2021-05-18 15:46:57 -0400

    ci: disable CodeCov
    Asana: https://app.asana.com/0/search/1200350119054966/1200274986747497

--- semaphore/test.sh
+++ semaphore/test.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-export ELEVATOR_CODECOV_TOKEN="1683abbb-241b-4ef8-9033-6806ae192b88"
 npm run test
-bash <(curl -s https://codecov.io/bash) -t $ELEVATOR_CODECOV_TOKEN -cF javascript
